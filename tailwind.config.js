374eb551ee84d885970524695bccacdd109dfec4
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-01-25 20:22:48 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
