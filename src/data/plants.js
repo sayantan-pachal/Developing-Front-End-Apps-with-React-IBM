2ebc201506db5236c7fc3c1a1100a2b995d88d81
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-01-25 20:42:46 +0530

    update plants data

---
+++ src/data/plants.js
@@ -0,0 +1,46 @@
+const plants = [
+  {
+    id: 1,
+    name: "Snake Plant",
+    price: 299,
+    category: "Indoor",
+    image: "/plants/snake.jpg",
+  },
+  {
+    id: 2,
+    name: "Peace Lily",
+    price: 399,
+    category: "Indoor",
+    image: "/plants/lily.jpg",
+  },
+  {
+    id: 3,
+    name: "Aloe Vera",
+    price: 199,
+    category: "Medicinal",
+    image: "/plants/aloe.jpg",
+  },
+  {
+    id: 4,
+    name: "Tulsi",
+    price: 149,
+    category: "Medicinal",
+    image: "/plants/tulsi.jpg",
+  },
+  {
+    id: 5,
+    name: "Rose",
+    price: 249,
+    category: "Outdoor",
+    image: "/plants/rose.jpg",
+  },
+  {
+    id: 6,
+    name: "Money Plant",
+    price: 179,
+    category: "Indoor",
+    image: "/plants/money.jpg",
+  },
+]
+
+export default plants
