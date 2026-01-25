4d877a380fb1a42a7a58008fdaa765831fc95e62
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-01-25 20:44:14 +0530

    update LandingPage

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 function Landing() {
   return (
-    <div>Landing</div>
+    <div
+      style={{
+        backgroundImage: "url('/plants/bg.jpg')",
+        height: "100vh",
+        backgroundSize: "cover",
+        color: "white",
+        textAlign: "center",
+        paddingTop: "150px",
+      }}
+    >
+      <h1>Paradise Nursery</h1>
+      <p>Your one-stop destination for healthy and happy houseplants.</p>
+      <Link to="/products">
+        <button style={{ padding: "10px 20px", fontSize: "16px" }}>
+          Get Started
+        </button>
+      </Link>
+    </div>
   )
 }
 
