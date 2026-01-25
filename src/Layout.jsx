a8352df35b500d209a756ed8d8953d9a52236772
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-01-25 20:41:55 +0530

    update

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,16 +3,14 @@ import React from 'react'
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
 import {Outlet} from 'react-router-dom'
-import ScrollToTop from './ScrollToTop'
 
 function Layout() {
     return (
-        <dev>
+        <div>
             <Header />
-            <ScrollToTop />
             <Outlet />
             <Footer />
-        </dev>
+        </div>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Layout from './Layout.jsx'
+import Layout from './Layout'
+import Home from './Components/Home/Home'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
-      <Route path='about' element={<About />} />
+      {/* <Route path='about' element={<About />} />
       <Route path='education' element={<Education />} />
       <Route path='project' element={<Project />} />
       <Route path='contact' element={<Contact />} />
       <Route path='blog' element={<Blog />} />
-      <Route path='resume' element={<Resume />} />
+      <Route path='resume' element={<Resume />} /> */}
       <Route path='*' element={<div>Not Found</div>} />
     </Route>
   )
