cc2bc45227ca04fa38ea5dc86b067a6d1b5cedb2
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-01-29 12:06:31 +0530

    update footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="bg-gradient-to-r from-gray-900 via-green-900 to-emerald-900 text-gray-200 mt-0">
       <div className="max-w-7xl mx-auto px-6 py-8 text-center space-y-3">
-        
+
         <h3 className="text-lg font-semibold text-white">
           🌿 Paradise Nursery
         </h3>
@@ -13,6 +13,17 @@ function Footer() {
 
         <p className="text-xs text-gray-400">
           © {new Date().getFullYear()} Paradise Nursery. All rights reserved.
+        </p>
+        <p className="text-gray-400 text-xs">
+          Created by{" "}
+          <a
+            href="https://sayantanpachal.vercel.app"
+            target="_blank"
+            rel="noreferrer"
+            className="text-gray-400 hover:underline"
+          >
+            Sayantan Pachal
+          </a>
         </p>
       </div>
     </footer>
